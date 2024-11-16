7bb376c77cebbb1d78fa303e46ec3fd6ed41f5f5
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2024-11-16 20:03:32 +0530

    Time: 75 ms (55.91%) | Memory: 60.2 MB (26.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -4,35 +4,29 @@
  */
 
 
-let Bfs = function (row, col, visited, rowLength, colLength, grid) {
+let Dfs = function (row, col, visited, rowLength, colLength, grid) {
   visited[row][col] = 1;
-  let queue = [];
-  queue.push([row, col]);
 
-  while (queue.length) {
-    const [currRow, currCol] = queue.shift();
+  const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
 
-    const directions = [
-      [1, 0],
-      [-1, 0],
-      [0, 1],
-      [0, -1],
-    ];
-
-    for (let [delrow, delcol] of directions) {
-      let neigbourRow = currRow + delrow;
-      let neigbourCol = currCol + delcol;
-      if (
-        neigbourRow >= 0 &&
-        neigbourRow < rowLength &&
-        neigbourCol >= 0 &&
-        neigbourCol < colLength &&
-        grid[neigbourRow][neigbourCol] === "1" &&
-        !visited[neigbourRow][neigbourCol]
-      ) {
-        visited[neigbourRow][neigbourCol] = 1;
-        queue.push([neigbourRow, neigbourCol]);
-      }
+  for (let [delrow, delcol] of directions) {
+    let neigbourRow = row + delrow;
+    let neigbourCol = col + delcol;
+    if (
+      neigbourRow >= 0 &&
+      neigbourRow < rowLength &&
+      neigbourCol >= 0 &&
+      neigbourCol < colLength &&
+      grid[neigbourRow][neigbourCol] === "1" &&
+      !visited[neigbourRow][neigbourCol]
+    ) {
+      visited[neigbourRow][neigbourCol] = 1;
+      Dfs(neigbourRow, neigbourCol, visited, rowLength, colLength, grid);
     }
   }
 };
@@ -47,9 +41,10 @@ var numIslands = function (grid) {
     for (let col = 0; col < n; col++) {
       if (!visited[row][col] && grid[row][col] === "1") {
         count++;
-        Bfs(row, col, visited, m, n, grid);
+        Dfs(row, col, visited, m, n, grid);
       }
     }
   }
+
   return count;
 };
