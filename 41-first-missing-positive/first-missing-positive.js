4d56d8153f628ebfc1833935b23441ee562f4bc3
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2025-01-13 22:38:00 +0530

    Time: 24 ms (46.90%) | Memory: 70.2 MB (14.82%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+     let n = nums.length;
+     let i = 0;
+     if(nums.length === 1){
+        if(nums[0] === 1 || nums[0] === 0)return nums[0]+1
+        else return 1;
+     }
+    const numSet = new Set(nums);
+    for (i = 1; i <= n; i++) {
+    if (!numSet.has(i)) {
+      return i;
+    }
+  }
+  return i;
+};
