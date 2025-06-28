1a5af2df63b83d4354da3e942216ad3ef2ee9e12
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2025-06-28 12:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 59.2 MB (53.75%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+function getIndex(index, arr) {
+  let i = index;
+  let j = i + 1;
+  while (i < arr.length && j < arr.length) {
+    if (arr[i] !== arr[j]) return j;
+    i++;
+    j++;
+  }
+  return j;
+}
+
+function getSubsets(index, arr = [], ans = [], res = []){
+    if(index === arr.length){
+        ans.push([...res]);
+        return;
+    }
+    res.push(arr[index]);
+    getSubsets(index + 1, arr, ans, res);
+    res.pop();
+    index = getIndex(index,arr);
+    getSubsets(index, arr, ans, res);
+
+}
+
+
+var subsetsWithDup = function(nums) {
+    let ans = [];
+    let res = [];
+    let index = 0;
+    nums.sort((a, b) => a - b);
+    getSubsets(index, nums, ans,res);
+    return ans;
+};
