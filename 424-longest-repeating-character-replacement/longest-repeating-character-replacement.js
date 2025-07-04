17b1d523aa0a51ebd14c34071e542f49bdc01f9a
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2025-07-04 16:24:24 +0530

    Time: 19 ms (60.34%) | Memory: 55.5 MB (83.39%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -4,45 +4,27 @@
  * @return {number}
  */
 
-function IntializeMap(s) {
-  let m = new Map();
-  for (let i = 0; i < s.length; i++) {
-    m.set(s[i], 0);
-  }
-  return m;
-}
 
-function getMaxFreq(m) {
-  let max = -1;
-  for (let val of m.values()) {
-    max = Math.max(max, val);
-  }
-  return max;
-}
 
 var characterReplacement = function(s, k) {
-  let m = IntializeMap(s);
-  let l = 0;
-  let r = 0;
-  let maxfreq = 0;
-  let maxLen = 0;
-  let n = s.length;
+   let m = new Map();
+  let l = 0,
+    maxFreq = 0,
+    maxLen = 0;
 
-  while (r < n) {
-    m.set(s[r], m.get(s[r]) ? m.get(s[r]) + 1 : 1);
-    maxfreq = getMaxFreq(m);
+  for (let r = 0; r < s.length; r++) {
+    m.set(s[r], (m.get(s[r]) || 0) + 1);
+    maxFreq = Math.max(maxFreq, m.get(s[r]));
 
-    if (r - l + 1 - maxfreq > k) {
+    if (r - l + 1 - maxFreq > k) {
       m.set(s[l], m.get(s[l]) - 1);
       l++;
-      maxfreq = getMaxFreq(m);
     }
 
-    if (r - l + 1 - maxfreq <= k) {
-      maxLen = Math.max(maxLen, r - l + 1);
-    }
-    r++;
+    maxLen = Math.max(maxLen, r - l + 1);
   }
 
   return maxLen;
+
+
 };
