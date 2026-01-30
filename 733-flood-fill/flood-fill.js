d359a8c4a0b7687fc4815545597f1794d12f6bba
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-01-30 17:41:50 +0530

    Time: 7 ms (5.22%) | Memory: 60.2 MB (5.95%) - LeetSync

--- 733-flood-fill/flood-fill.js
+++ 733-flood-fill/flood-fill.js
@@ -6,57 +6,30 @@
  * @return {number[][]}
  */
 
-const Dfs = function (
-  grid,
-  gridCopy,
-  sr,
-  sc,
-  newColor,
-  initialColor,
-  rowLength,
-  colLength
-) {
-  gridCopy[sr][sc] = newColor;
-
-  const directions = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-
-  for (let [delrow, delcol] of directions) {
-    const neigbourRow = delrow + sr;
-    const neigbourCol = delcol + sc;
-
-    if (
-      neigbourRow >= 0 &&
-      neigbourRow < rowLength &&
-      neigbourCol >= 0 &&
-      neigbourCol < colLength &&
-      grid[neigbourRow][neigbourCol] === initialColor &&
-      gridCopy[neigbourRow][neigbourCol] !== newColor
-    ) {
-      Dfs(
-        grid,
-        gridCopy,
-        neigbourRow,
-        neigbourCol,
-        newColor,
-        initialColor,
-        rowLength,
-        colLength
-      );
+function checkfloodfill(image, newImage, visited, newColor, sr, sc){
+    let n = image.length;
+    let m = image[0].length;
+    visited[sr][sc] = true;
+    newImage[sr][sc] = newColor;
+    const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    for (const [dr, dc] of directions) {
+      const nr = sr + dr;
+      const nc = sc + dc;
+      
+      if (nr >= 0 && nr < n && nc >= 0 && nc < m && 
+          image[nr][nc] === image[sr][sc] && !visited[nr][nc]) {
+            checkfloodfill(image, newImage, visited, newColor, nr, nc);
+          }
     }
-  }
-};
+    
+}
 
 var floodFill = function(image, sr, sc, color) {
-  let m = image.length;
-  let n = image[0].length;
-  let gridCopy = image.map((row) => [...row]);
-  let initialColor = image[sr][sc];
-  if (initialColor === color) return gridCopy;
-  Dfs(image, gridCopy, sr, sc, color, initialColor, m, n);
-  return gridCopy;
+    const rows = image.length;
+    const cols = image[0].length;
+    let newColor = color;
+    const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
+    const newImage = Array.from({length: rows}, (_, i) => [...image[i]]);
+    checkfloodfill(image, newImage, visited, newColor, sr, sc);
+    return newImage;
 };
