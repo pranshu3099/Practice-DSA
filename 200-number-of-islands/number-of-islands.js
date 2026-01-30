2a9cb9ce47d2ae4403804f1c984d25b8a6b730ef
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-01-30 16:49:28 +0530

    Time: 75 ms (35.11%) | Memory: 66.5 MB (22.36%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -2,49 +2,40 @@
  * @param {character[][]} grid
  * @return {number}
  */
-
-
-let Dfs = function (row, col, visited, rowLength, colLength, grid) {
-  visited[row][col] = 1;
-
-  const directions = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-
-  for (let [delrow, delcol] of directions) {
-    let neigbourRow = row + delrow;
-    let neigbourCol = col + delcol;
-    if (
-      neigbourRow >= 0 &&
-      neigbourRow < rowLength &&
-      neigbourCol >= 0 &&
-      neigbourCol < colLength &&
-      grid[neigbourRow][neigbourCol] === "1" &&
-      !visited[neigbourRow][neigbourCol]
-    ) {
-      visited[neigbourRow][neigbourCol] = 1;
-      Dfs(neigbourRow, neigbourCol, visited, rowLength, colLength, grid);
+function getNumberOfIslands(grid, visited, i, j){
+    let n = grid.length;
+    let m = grid[0].length;
+    let queue = [];
+    visited[i][j] = true
+    queue.push([i,j]);
+    while(queue.length > 0){
+        let [row, col] = queue.shift();
+        const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+        for(const [delrow, delcol] of directions){
+            let nrow = row + delrow;
+            let ncol  = col + delcol;
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
+            && grid[nrow][ncol] === "1" && !visited[nrow][ncol]){
+                visited[nrow][ncol] = true;
+                queue.push([nrow, ncol])
+            }
+        }
     }
-  }
-};
+}
 
 
-var numIslands = function (grid) {
-  let m = grid.length;
-  let n = grid[0].length;
-  let visited = Array.from({ length: m }, () => Array(n).fill(0));
-  let count = 0;
-  for (let row = 0; row < m; row++) {
-    for (let col = 0; col < n; col++) {
-      if (!visited[row][col] && grid[row][col] === "1") {
-        count++;
-        Dfs(row, col, visited, m, n, grid);
-      }
+var numIslands = function(grid) {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const visited = Array.from({length:rows},() => Array(cols).fill(false));
+    let count = 0;
+    for(let i = 0 ; i<rows ; i++){
+        for(let j = 0; j<cols; j++){
+            if(grid[i][j] === "1" && !visited[i][j]){
+                count++;
+                getNumberOfIslands(grid, visited, i, j);
+            }
+        }
     }
-  }
-
-  return count;
+    return count;
 };
