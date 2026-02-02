a11a502046282ad62db8e5210a2d308aa3aec647
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-02-02 15:19:48 +0530

    Time: 66 ms (64.10%) | Memory: 72.7 MB (36.67%) - LeetSync

--- 542-01-matrix/01-matrix.js
+++ 542-01-matrix/01-matrix.js
@@ -2,47 +2,42 @@
  * @param {number[][]} mat
  * @return {number[][]}
  */
-var updateMatrix = (mat) => {
-  let m = mat.length;
-  let n = mat[0].length;
-  let queue = [];
-  let gridCopy = mat.map((row) => [...row]);
-  let visited = Array.from({ length: m }, () => Array(n).fill(0));
-  let distance = Array.from({ length: m }, () => Array(n).fill());
 
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (gridCopy[i][j] === 0) {
-        queue.push([i, j, 0]);
-        visited[i][j] = 1;
-      }
+function zeroOneMatrix(mat, distance, visited, queue){
+    const n = mat.length;
+    const m = mat[0].length
+    let count = 0;
+    while(count < queue.length){
+        const [r, c, step] = queue[count++];
+        distance[r][c] = step;
+        const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+        for(const [dr, dc] of directions){
+            const nr = dr + r;
+            const nc = dc + c;
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m && 
+                !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    queue.push([nr, nc, step + 1])
+          }
+        }
     }
-  }
+}
 
-  while (queue.length) {
-    const [currRow, currCol, currDist] = queue.shift();
-    distance[currRow][currCol] = currDist;
-    let directions = [
-      [1, 0],
-      [-1, 0],
-      [0, 1],
-      [0, -1],
-    ];
 
-    for (const [delrow, delcol] of directions) {
-      const neigbourRow = delrow + currRow;
-      const neigbourCol = delcol + currCol;
-      if (
-        neigbourRow >= 0 &&
-        neigbourRow < m &&
-        neigbourCol >= 0 &&
-        neigbourCol < n &&
-        visited[neigbourRow][neigbourCol] !== 1
-      ) {
-        queue.push([neigbourRow, neigbourCol, currDist + 1]);
-        visited[neigbourRow][neigbourCol] = 1;
-      }
+var updateMatrix = function(mat) {
+    const row = mat.length;
+    const col = mat[0].length;
+    const distance = Array.from({length: row}, (_, i) => [...mat[i]]);
+    const visited = Array.from({length:row}, () => Array(col).fill(false));
+    const queue = [];
+    for(let i = 0; i< row ; i++){
+        for(let j = 0 ; j< col; j++){
+            if(mat[i][j] === 0){
+                visited[i][j] = true;
+                queue.push([i, j, 0]);
+            }
+        }
     }
-  }
-  return distance;
+    zeroOneMatrix(mat, distance, visited, queue)
+    return distance;
 };
