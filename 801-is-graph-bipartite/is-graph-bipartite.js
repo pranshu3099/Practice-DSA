0b1a6cdd05c8cdee1c6e32ecfcd4d55f075d0768
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-02-03 16:18:00 +0530

    Time: 3 ms (43.22%) | Memory: 58.2 MB (32.63%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.js
+++ 801-is-graph-bipartite/is-graph-bipartite.js
@@ -3,35 +3,34 @@
  * @return {boolean}
  */
 
-let checkIfBipartite = function(graph, n){
-    let color = new Array(n).fill(null).map(()=>-1);
-      for (let i = 0; i < n; i++) {
-      if (color[i] === -1) {
-        let queue = [];
-        queue.push(i);
-        color[i] = 0;
-        let index = 0;
-        while (index < queue.length) {
-          let parent = queue[index];
-          index++;
-          let parentColor = color[parent];
-          for (const node of graph[parent]) {
-            if (color[node] === -1) {
-              color[node] = parentColor === 1 ? 0 : 1;
-              queue.push(node);
-            } else {
-              if (color[node] === parentColor) return false;
-            }
-          }
+function bfs(graph, color, startingNode){
+    const queue = [startingNode];
+    color[startingNode] = 0;
+    let start = 0;
+    while(start < queue.length){
+        const node = queue[start++];
+        for(const adj of graph[node] || []){
+            if(color[adj] === -1){
+                color[adj] = 1 - color[node];
+                queue.push(adj)
+            }else if(color[adj] === color[node]) return false;
         }
-      }
     }
+
     return true;
+
 }
 
-
 var isBipartite = function(graph) {
-    let n = graph.length;
-    let ans = checkIfBipartite(graph, n)
-    return ans;
+    const n = graph.length;
+    const color = new Array(n).fill(-1);
+    
+    for(let i = 0; i < n; i++){
+        if(color[i] === -1){ 
+            if(!bfs(graph, color, i)){ 
+                return false; 
+            }
+        }
+    }
+    return true;
 };
