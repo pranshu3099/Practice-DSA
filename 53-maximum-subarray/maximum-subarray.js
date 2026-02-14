ba2e49ca5a48b1799bca6760866b358c615a2755
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-02-14 18:57:23 +0530

    Time: 3 ms (43.68%) | Memory: 64.1 MB (49.94%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -5,18 +5,14 @@
 var maxSubArray = function(nums) {
     if(nums.length === 1) return nums[0];
     let n = nums.length;
-    let i = 1;
     let sum = nums[0];
     let max = nums[0];
-    while(i < n){
-        sum = sum + nums[i]
-        if(sum < nums[i]){
-           sum = nums[i];
-           
-        }
-        max  = Math.max(sum, max);
-        i++; 
+    for(let i = 1; i < n; i++){
+    sum = sum + nums[i];
+    if(sum < nums[i]){
+       sum = nums[i];
     }
-
+    max = Math.max(sum, max);
+}
     return max;
 };
