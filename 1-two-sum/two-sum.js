3d7ee04906d04225eca04ca19b413a53c1566377
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-03-01 13:08:48 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (28.21%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   let seen = {};
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (seen.hasOwnProperty(complement)) {
-      return [seen[complement], i];
-    } else {
-      seen[nums[i]] = i;
+    let seen = {};
+    for(let i = 0; i<nums.length;i++){
+        let complement = target - nums[i];
+        if(seen.hasOwnProperty(complement)){
+            return [seen[complement], i]
+        }else{
+            seen[nums[i]] = i;
+        }
     }
-  }
 };
