91fe5fdd764c4e5cdead250cae655f65967acecf
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-03-20 12:19:25 +0530

    Time: 2 ms (70.16%) | Memory: 60.7 MB (18.93%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function(nums) {
-    let max = 0;
-    let count = 0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] === 1){
-            count++;
-            max = Math.max(count,max)
-        }
-        else{
-            count = 0;
-        }
+  const n = nums.length;
+  let count = 0;
+  let max = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      count = count + 1;
+      max = Math.max(count, max);
+    } else {
+      count = 0;
     }
-    return max;
+  }
+  return max;
 };
