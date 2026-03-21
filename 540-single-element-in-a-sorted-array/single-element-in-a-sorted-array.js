0a6e862be461f09591c68d951176daa36a374d8d
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-03-21 12:39:16 +0530

    Time: 1 ms (16.62%) | Memory: 56.7 MB (88.33%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -3,17 +3,22 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-  let n = nums.length;
-  let obj = {};
-  for (let i = 0; i < n; i++) {
-    if (obj[nums[i]]) {
-      obj[nums[i]]++;
-    } else {
-      obj[nums[i]] = 1;
-    }
-  }
+//   let n = nums.length;
+//   let obj = {};
+//   for (let i = 0; i < n; i++) {
+//     if (obj[nums[i]]) {
+//       obj[nums[i]]++;
+//     } else {
+//       obj[nums[i]] = 1;
+//     }
+//   }
 
-  for (let key in obj) {
-    if (obj[key] === 1) return Number(key);
-  }
+//   for (let key in obj) {
+//     if (obj[key] === 1) return Number(key);
+//   }
+let xor = 0;
+for(let i = 0 ; i<nums.length;i++){
+    xor = xor ^ nums[i];
+}
+return xor;
 };
