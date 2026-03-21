924609af9189206222807140e5c7690fdcce33ff
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-03-21 12:35:23 +0530

    Time: 7 ms (5.60%) | Memory: 62.6 MB (5.04%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -3,31 +3,17 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-let n = nums.length;
-  if (n === 1) return nums[0];
-  if (nums[0] !== nums[1]) return nums[0];
-  if (nums[n - 1] !== nums[n - 2]) return nums[n - 1];
-  let low = 1;
-  let high = n - 2;
-  let ans = -1;
-     while (low <= high) {
-    let middle = Math.floor((low + high) / 2);
-
-    if (
-      nums[middle] !== nums[middle - 1] &&
-      nums[middle] !== nums[middle + 1]
-    ) {
-      ans = nums[middle];
-      break;
-    }
-    if (
-      (middle % 2 === 1 && nums[middle] === nums[middle - 1]) ||
-      (middle % 2 === 0 && nums[middle] === nums[middle + 1])
-    ) {
-      low = middle + 1;
+  let n = nums.length;
+  let obj = {};
+  for (let i = 0; i < n; i++) {
+    if (obj[nums[i]]) {
+      obj[nums[i]]++;
     } else {
-      high = middle - 1;
+      obj[nums[i]] = 1;
     }
   }
-  return ans;
+
+  for (let key in obj) {
+    if (obj[key] === 1) return Number(key);
+  }
 };
