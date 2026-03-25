74d091641988c863b1183fade8125bcf7a18a742
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-03-25 23:31:31 +0530

    Time: 33 ms (92.35%) | Memory: 81.1 MB (34.13%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -2,27 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-function findElement(set, x) {
-  if (set.has(x)) return true;
-  else return false;
-}
 
 var longestConsecutive = function(nums) {
-      let set = new Set(nums);
-  let count = 1;
-  let maxCount = 0;
-  for (let item of set) {
-    if (findElement(set, item - 1)) {
-    } else {
-      let num = item;
-      count = 1;
-      while (set.has(num + 1)) {
-        count = count + 1;
-        num = num + 1;
-      }
-    }
-    maxCount = Math.max(count, maxCount);
-  }
+    const set = new Set(nums);
+    let maxCount = 0;
 
-  return maxCount;
+    for (let item of set) {
+        if (!set.has(item - 1)) {  
+            let num = item;
+            let count = 1;
+            while (set.has(num + 1)) {
+                count++;
+                num++;
+            }
+            maxCount = Math.max(count, maxCount);
+        }
+    }
+
+    return maxCount;
 };
