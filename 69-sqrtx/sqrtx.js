bf6222b6a142df7c621f4f4a381560e17b0ed257
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-06-02 11:51:08 +0530

    Time: 1 ms (73.49%) | Memory: 56.9 MB (27.95%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -5,16 +5,13 @@
 var mySqrt = function(x) {
   let low = 1;
   let high = x;
-  let ans = 0;
   while (low <= high) {
-    let middle = Math.floor((low + high) / 2);
-    let val = middle * middle;
-    if (val > x) {
-      high = middle - 1;
-    } else if (val <= x) {
-      ans = Math.max(middle, ans);
-      low = middle + 1;
+    let mid = Math.floor((low + high) / 2);
+    if ((mid * mid) <= x) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
     }
   }
-  return ans;
+  return high;
 };
