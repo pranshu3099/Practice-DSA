53eb37ddc374202be2c00c42587fdd817aa24194
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-07-07 11:51:11 +0530

    Time: 5 ms (20.77%) | Memory: 59.6 MB (77.89%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,26 +3,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-  let n = nums.length;
-  let start = -1;
   let j = 0;
-  for (let i = 0; i < n; i++) {
-    if (nums[i] === 0) {
-      start = i;
-      break;
-    }
-  }
-  if(start === -1)return nums;
-  j = start + 1;
-  while (j < n) {
-    if (nums[start] === 0 && nums[j] !== 0) {
-      let temp = nums[start];
-      nums[start] = nums[j];
-      nums[j] = temp;
-      start++;
-      j++;
-    } else if (nums[j] === 0) {
-      j++;
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i]!==0){
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+        j++;
     }
   }
   return nums;
