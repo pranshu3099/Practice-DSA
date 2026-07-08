28bf589e756de0f504fa5fbcdc26004af3ad81b2
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-07-08 14:58:02 +0530

    Time: 3 ms (48.52%) | Memory: 61.9 MB (88.65%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0 ;
+    while(left < right){
+        let width = right - left;
+        let currentHeight = Math.min(height[left], height[right]);
+        let area = width * currentHeight;
+        maxWater = Math.max(maxWater, area)
+        if(height[left] < height[right]){
+            left++;
+        }else{
+            right--;
+        }
+    }
+    return maxWater;
+};
