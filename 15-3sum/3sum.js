96b5533cbb7eb26f1b169889c0f853cf63ab7ba0
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-07-09 16:13:09 +0530

    Time: 40 ms (32.84%) | Memory: 73.6 MB (10.66%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -2,31 +2,33 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-   let ans = [];
-   nums.sort((a,b)=>a-b);
-   for(let i=0;i<nums.length;i++){
-    if(i > 0 && nums[i] === nums[i-1])continue;
-    else{
-        let j = i+1;
-        let k = nums.length-1;
-       while(j<k){
-         let sum = nums[i] + nums[j] + nums[k]
-        if(sum <0){
-            j++
+var threeSum = function (nums = []) {
+  nums.sort((a, b) => a - b);
+  if (nums[0] > 0) return [];
+  let n = nums.length;
+  let ans = [];
+  for (let i = 0; i < n; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = n - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        ans.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+        while (nums[left] === nums[left - 1]) {
+          left++;
         }
-        else if(sum>0){
-            k--;
+        while (nums[right] === nums[right + 1]) {
+          right--;
         }
-        else{
-           ans.push([nums[i],nums[j], nums[k]]);
-           j++;
-           k--;
-           while(j<k && nums[j]===nums[j-1])j++;
-           while(j<k && nums[k]=== nums[k+1])k--;
-        }
-       }
+      } else if (sum < 0) {
+        left++;
+      } else if (sum > 0) {
+        right--;
+      }
     }
-   }
-   return ans;
+  }
+  return ans;
 };
