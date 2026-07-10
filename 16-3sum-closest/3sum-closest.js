3617ddd7ff6ce645efaac67a07b6fc4d17f3d7d6
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-07-10 12:23:47 +0530

    Time: 17 ms (18.02%) | Memory: 57.7 MB (15.86%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b);
+    let n = nums.length;
+    let closest = nums[0] + nums[1] + nums[2];
+    for(let i = 0; i < nums.length - 2 ; i++){
+        let left = i + 1;
+        let right = n - 1;
+        while(left < right){
+            let sum = nums[i] + nums[left] + nums[right];
+            if(Math.abs(sum - target) < Math.abs(closest - target)){
+                closest = sum;
+            }if(sum < target){
+                left++
+            }else if(sum > target){
+                right--;
+            }else{
+                return sum;
+            }
+        }
+    }
+    return closest;
+};
